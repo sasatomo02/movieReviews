6d87704e6eff494307d6ecdd649439b9564a151a
STCN2\n1230012 <sanetomo0215@gmail.com>
2025-05-18 16:46:12 +0900

    Fix: Use  for Heroku

--- src/main/java/com/example/movie/MovieApplication.java
+++ src/main/java/com/example/movie/MovieApplication.java
@@ -2,6 +2,9 @@ package com.example.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MovieApplication {
@@ -10,4 +13,13 @@ public class MovieApplication {
 		SpringApplication.run(MovieApplication.class, args);
 	}
 
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+		return server -> {
+			String port = System.getenv("PORT");
+			if (port != null && !port.isEmpty()) {
+				server.setPort(Integer.parseInt(port));
+			}
+		};
+	}
 }
