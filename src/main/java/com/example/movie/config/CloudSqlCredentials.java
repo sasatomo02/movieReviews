97632d941d9c903e617e40df972b407eb8dd283e
STCN2\n1230012 <sanetomo0215@gmail.com>
2025-05-29 16:41:20 +0900

    a

--- src/main/java/com/example/movie/config/CloudSqlCredentials.java
+++ src/main/java/com/example/movie/config/CloudSqlCredentials.java
@@ -5,6 +5,9 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Component
 public class CloudSqlCredentials {
@@ -12,20 +15,12 @@ public class CloudSqlCredentials {
     @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
     private String encodedCredentials;
 
-    public JSONObject getCredentials() throws JSONException {
+    public JSONObject getCredentials() throws JSONException, IOException {
+        // ファイルから読み込むのではなく、環境変数の文字列を直接デコード
         byte[] decodedBytes = Base64.decodeBase64(encodedCredentials);
         String decodedString = new String(decodedBytes);
         return new JSONObject(decodedString);
     }
 
-    // 必要に応じて、個別の値を取得するメソッドを追加できます
-    public String getPrivateKey() throws JSONException {
-        return getCredentials().getString("private_key");
-    }
-
-    public String getClientEmail() throws JSONException {
-        return getCredentials().getString("client_email");
-    }
-
-    // ... 他の必要なフィールド
+    // ... (必要に応じて他のメソッド)
 }
