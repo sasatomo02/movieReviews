73e49f074de93c58e9160d89a2144ad7c3c228e4
STCN2\n1230012 <sanetomo0215@gmail.com>
2025-05-29 16:38:56 +0900

    a

--- src/main/java/com/example/movie/config/CloudSqlCredentials.java
+++ src/main/java/com/example/movie/config/CloudSqlCredentials.java
@@ -1,8 +1,8 @@
 package com.example.movie.config;
 
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,4 +17,15 @@ public class CloudSqlCredentials {
         String decodedString = new String(decodedBytes);
         return new JSONObject(decodedString);
     }
+
+    // 必要に応じて、個別の値を取得するメソッドを追加できます
+    public String getPrivateKey() throws JSONException {
+        return getCredentials().getString("private_key");
+    }
+
+    public String getClientEmail() throws JSONException {
+        return getCredentials().getString("client_email");
+    }
+
+    // ... 他の必要なフィールド
 }
